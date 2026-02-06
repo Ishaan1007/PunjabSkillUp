2c600ae6de035b873b94276518d3d2307e1bcf82
Ishaan1007 <ishaanjain4u@gmail.com>
2026-02-07 00:09:15 +0530

    Update courses

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,5 @@
 export const contact = {
-  email: 'info.punjabupskill@gmail.com',
-  phone: '9468512XXX',
-  address: ['building no. 5,', 'patiala, punjab']
+  email: 'ishaanjain4u@gmail.com',
+  phone: '9463476799',
+  address: ['Punjabi University', 'Patiala, Punjab']
 }
